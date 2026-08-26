546f3c766d32875a5c216cb4e218f3c704d65160
Omar Taima <itzdiaz2018@gmail.com>
2026-08-26 10:14:28 +0300

    Update page.tsx

--- src/routes/Projects/page.tsx
+++ src/routes/Projects/page.tsx
@@ -423,7 +423,7 @@ const ProjectsPage: React.FC = () => {
                 </div>
             ) : (
                 <DndProvider backend={HTML5Backend}>
-                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-6">
                     {filtered.map((project: any) => (
                         <SortableProjectCard
                             key={getProjectId(project)}
